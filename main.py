f2d85ae96474032b81d47ddafa9a53b59de5db0a
Anya <anna.artamono@gmail.com>
2022-12-10 01:14:36 +0300

    На странице /shortest_path создан режим добавления вершин графа

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import os
 
-from flask import Flask, render_template_string, render_template
+from forms.getPoints import GetPointsForm
+from flask import Flask, render_template_string, render_template, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'JHDBOsdhc98dbHBASJwxkOaw89d3bq2'
 
 
 @app.route('/')
 def main():
-    return render_template("main.html")
+    return render_template('main.html')
+
+
+@app.route('/shortest_path', methods=['GET', 'POST'])
+def shortest_path():
+    form = GetPointsForm()
+    if form.is_submitted():
+        points = form.points.data
+        return render_template("shortest_path_get.html")
+    else:
+        return render_template('for_tests.html', form=form)
+
+
+
 
 
 if __name__ == '__main__':
